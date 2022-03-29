8fe776ec5608a5ef89dc83374d87f5871efc7fdc
rider <myintthwaykhine127@gmail.com>
2022-03-29 21:54:18 +0630

    create name filter using ref computed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,65 +1,32 @@
 <template>
   <div class="home">
-    <h1>Ref Vs Reactive</h1>
-
-    <!--use Ref-->
-    <div class="box">
-      <h2 class="title">Use Ref</h2>
-      <h2>Name : {{ personOne.name }}</h2>
-      <h2>Age : {{ personOne.age }}</h2>
-      <h3>Normal name : {{ nameOne }}</h3>
-      <button @click="changeNameOne">Change Name</button>
-    </div>
-
-    <div class="box">
-      <h2 class="title">Use Ref</h2>
-      <h2>Name : {{ personTwo.name }}</h2>
-      <h2>Age : {{ personTwo.age }}</h2>
-      <h3>Normal name : {{ nameTwo }}</h3>
-      <button @click="changeNameTwo">Change Name</button>
+    <h1>Names Filter</h1>
+    <input type="text" v-model="search" /><br />
+    search item - {{ search }}
+    <div v-for="name in filterName" :key="name">
+      {{ name }}
     </div>
   </div>
 </template>
 
 <script>
-/*
-  ref can change eveny things but reactive can change object and array only;
-*/
-import { reactive, ref } from "@vue/reactivity";
+import { computed, ref } from "@vue/runtime-core";
 export default {
   setup() {
-    let personOne = ref({ name: "rider", age: 21 });
-    let nameOne = ref("Rider");
-    let changeNameOne = () => {
-      personOne.value.name = "Myint Thway Khine";
-      nameOne.value = "Change Name One";
-    };
-
-    let personTwo = reactive({ name: "kyawkyaw", age: 21 });
-    let nameTwo = reactive("Kyaw Kyaw"); //with error value cannot be made reactive: Kyaw Kyaw
-    let changeNameTwo = () => {
-      personTwo.name = "Myint Thway Khine";
-    };
-    return {
-      personOne,
-      changeNameOne,
-      personTwo,
-      changeNameTwo,
-      nameOne,
-      nameTwo,
-    };
+    let search = ref("");
+    let names = ref(["aungaung", "rider", "kyawkyaw", "susu", "ayeaye"]);
+    let filterName = computed(() => {
+      return names.value.filter((n) => {
+        return n.includes(search.value);
+      });
+    });
+    return { names, search, filterName };
   },
 };
 </script>
 
-<style scoped>
-.box {
-  border: 1px solid;
-  width: 300px;
-  margin: 60px auto;
-  padding: 20px;
-}
-.title {
-  color: blue;
+<style>
+input {
+  margin: 20px auto;
 }
 </style>
