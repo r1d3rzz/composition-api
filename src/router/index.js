ce09bcd0a249a22ec387bcce6a64fa2bb6fa02ac
rider <myintthwaykhine127@gmail.com>
2022-04-13 15:29:58 +0630

    build create post frontend

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import PostDetail from "../views/PostDetail.vue";
+import PostCreate from "../views/PostCreate.vue";
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: "postDetail",
     component: PostDetail,
     props: true,
+  },
+  {
+    path: "/post/create",
+    name: "postCreate",
+    component: PostCreate,
   },
 ];
 
---
+++ src/views/PostCreate.vue
@@ -0,0 +1,116 @@
+<template>
+  <h1>Create Your Post</h1>
+
+  <div class="postCreate">
+    <form>
+      <div class="inputBox">
+        <label for="title" class="postLabel">Post Title</label>
+        <input
+          type="text"
+          id="title"
+          class="postInput"
+          v-model="title"
+          required
+        />
+      </div>
+
+      <div class="inputBox">
+        <label for="body" class="postLabel">Post Body</label>
+        <input
+          type="text"
+          id="body"
+          class="postInput"
+          v-model="body"
+          required
+        />
+      </div>
+
+      <div class="inputBox">
+        <label for="tag" class="postLabel">Tag(Enter add new Tag)</label>
+        <input
+          type="text"
+          id="tag"
+          class="postInput"
+          @keydown.enter.prevent="addNewTags"
+          v-model="tag"
+        />
+        <br />
+        <div v-for="tag in tags" :key="tag" class="tagContainer">
+          <span>{{ tag }}</span>
+        </div>
+      </div>
+
+      <div class="inputBox">
+        <button class="createBtn">Create Post</button>
+      </div>
+    </form>
+  </div>
+
+  <!-- Title - {{ title }} <br />
+  Body - {{ body }} <br />
+  Tag - {{ tag }} <br />
+  Tags - {{ tags }} -->
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    let title = ref("");
+    let body = ref("");
+    let tag = ref("");
+    let tags = ref([]);
+
+    let addNewTags = () => {
+      if (!tags.value.includes(tag.value)) {
+        tags.value.push(tag.value);
+      }
+      tag.value = "";
+    };
+
+    return { title, body, tag, addNewTags, tags };
+  },
+};
+</script>
+
+<style>
+.inputBox {
+  text-align: start;
+  max-width: 400px;
+  margin: 30px auto;
+}
+.postLabel {
+  font-size: 22px;
+  display: block;
+  font-weight: bold;
+  padding: 10px 0;
+}
+.postInput {
+  border: none;
+  width: 80%;
+  outline: none;
+  font-size: 22px;
+  border-bottom: 2px solid gray;
+}
+.createBtn {
+  border: none;
+  background-color: blue;
+  font-size: 15px;
+  border-radius: 5px;
+  padding: 5px 10px;
+  font-weight: bold;
+  color: white;
+  cursor: pointer;
+}
+.tagContainer {
+  width: 50px;
+  background-color: #fafa;
+  text-align: center;
+  padding: 5px 10px;
+  border-radius: 10px;
+  font-size: 18px;
+  display: inline-block;
+  margin-top: 10px;
+  margin-right: 10px;
+}
+</style>
