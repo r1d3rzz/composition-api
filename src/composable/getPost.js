93824c5c471e6e2c9ceaf6f0167c71ff10458a89
rider <myintthwaykhine127@gmail.com>
2022-04-15 15:52:34 +0630

    fetch single post data in firebase

--- src/composable/getPost.js
+++ src/composable/getPost.js
@@ -1,17 +1,13 @@
 import { ref } from "vue";
+import { db } from "../firebase/config";
 
 let getPost = (id) => {
   let post = ref(null);
   let error = ref("");
   let load = async () => {
     try {
-      // await new Promise((resolve, reject) => setTimeout(resolve, 2000));
-      let response = await fetch("http://localhost:3000/posts/" + id);
-      if (response === 404) {
-        throw new Error("URL IS WRONG");
-      }
-      let data = await response.json();
-      post.value = data;
+      let response = await db.collection("posts").doc(id).get();
+      post.value = { id: response.id, ...response.data() };
     } catch (err) {
       error.value = err.message;
     }
