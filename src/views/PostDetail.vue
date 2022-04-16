0fa54f6f39c5b3e03538e2247b2bd3019912f7b7
rider <myintthwaykhine127@gmail.com>
2022-04-16 09:13:43 +0630

    create delete post function

--- src/views/PostDetail.vue
+++ src/views/PostDetail.vue
@@ -1,6 +1,13 @@
 <template>
-  <div v-if="post" class="singlePost">
-    <h2>{{ post.title }}</h2>
+  <div v-if="post" class="singlePost detailSingle">
+    <div class="flexing">
+      <div>
+        <h2>{{ post.title }}</h2>
+      </div>
+      <div>
+        <button class="deleteBtn" @click="deletePost">Delete</button>
+      </div>
+    </div>
     <p>{{ post.body }}</p>
     <div v-for="tag in post.tags" class="tagContainer" :key="tag">
       <span>{{ tag }}</span>
@@ -12,24 +19,49 @@
 </template>
 
 <script>
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import LoadingSpinner from "../components/LoadingSpinner";
 import getPost from "../composable/getPost";
+import { db } from "../firebase/config";
 
 export default {
   components: { LoadingSpinner },
   props: ["id"],
   setup(prop) {
     let route = useRoute();
+    let router = useRouter();
 
     let { post, error, load } = getPost(route.params.id);
 
     load();
 
-    return { post, error };
+    let deletePost = async () => {
+      let id = prop.id;
+      await db.collection("posts").doc(id).delete();
+      router.push("/");
+    };
+
+    return { post, error, deletePost };
   },
 };
 </script>
 
 <style>
+.detailSingle {
+  max-width: 600px;
+  margin: 0 auto;
+}
+.flexing {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.deleteBtn {
+  background-color: red;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+}
 </style>
